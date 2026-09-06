e77762648a1eb2d298d26198049d0e7fa627b649
kesuytinh2005 <pg13042005null@gmail.com>
2026-09-06 20:29:21 +0700

    Initial Telegram bot

--- commands/getuidfb.py
+++ commands/getuidfb.py
@@ -148,21 +148,34 @@ LOG = logging.getLogger("getuidfb")
 # ======================================================================
 
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Linux; Android 15; Mobile) "
+    "authority": "www.facebook.com",
+    "accept": (
+        "text/html,"
+        "application/xhtml+xml,"
+        "application/xml;q=0.9,"
+        "image/avif,"
+        "image/webp,"
+        "image/apng,"
+        "*/*;q=0.8"
+    ),
+    "accept-language": (
+        "vi-VN,vi;q=0.9,"
+        "en-US;q=0.8,en;q=0.7"
+    ),
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "upgrade-insecure-requests": "1",
+    "user-agent": (
+        "Mozilla/5.0 "
+        "(Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 "
         "(KHTML, like Gecko) "
-        "Chrome/142.0.0.0 Mobile Safari/537.36"
+        "Chrome/139.0.0.0 "
+        "Safari/537.36"
     ),
-    "Accept": (
-        "text/html,application/xhtml+xml,application/xml;"
-        "q=0.9,image/avif,image/webp,*/*;q=0.8"
-    ),
-    "Accept-Language": "vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache",
-    "Upgrade-Insecure-Requests": "1",
-    "DNT": "1",
 }
 
 

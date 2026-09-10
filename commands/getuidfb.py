5c06894740e22dca5d501709c78ad38080db9e65
kesuytinh2005 <pg13042005null@gmail.com>
2026-09-10 18:47:47 +0700

    Initial Telegram bot

--- commands/getuidfb.py
+++ commands/getuidfb.py
@@ -200,27 +200,25 @@ def make_headers(
         else random.choice(USER_AGENTS[2:])
     )
     headers = {
-        "authority": "www.facebook.com",
-        "accept": (
-            "text/html,"
-            "application/xhtml+xml,"
-            "application/xml;q=0.9,"
-            "image/avif,"
-            "image/webp,"
-            "image/apng,"
-            "*/*;q=0.8"
-        ),
-        "accept-language": (
-            "vi-VN,vi;q=0.9,"
-            "en-US;q=0.8,en;q=0.7"
-        ),
-        "cache-control": "no-cache",
-        "pragma": "no-cache",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "upgrade-insecure-requests": "1",
-        "user-agent": ua,
+        'authority': 'www.facebook.com',
+		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+		'accept-language': 'vi,en-US;q=0.9,en;q=0.8',
+		'cache-control': 'max-age=0',
+		'dnt': '1',
+		'dpr': '1.25',
+		'sec-ch-ua': '"Chromium";v="117", "Not;A=Brand";v="8"',
+		'sec-ch-ua-full-version-list': '"Chromium";v="117.0.5938.157", "Not;A=Brand";v="8.0.0.0"',
+		'sec-ch-ua-mobile': '?0',
+		'sec-ch-ua-model': '""',
+		'sec-ch-ua-platform': '"Windows"',
+		'sec-ch-ua-platform-version': '"15.0.0"',
+		'sec-fetch-dest': 'document',
+		'sec-fetch-mode': 'navigate',
+		'sec-fetch-site': 'same-origin',
+		'sec-fetch-user': '?1',
+		'upgrade-insecure-requests': '1',
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+		'viewport-width': '1038',
     }
     if referer:
         headers["Referer"] = referer
